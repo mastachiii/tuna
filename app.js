afe6a2fbb3815d7813f2555adf4a002f0f61a37d
mastachii <mastachii273@gmail.com>
2024-12-20 02:34:14 +0800

    feat: Started Styling Homepage

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Middlewares
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use("/books", booksRouter);
 app.use((err, req, res, next) => {
